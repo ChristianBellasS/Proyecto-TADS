7dffd055ceabce329009e401c3c022113d63adb4
BellasS <cpbellasmil@gmail.com>
2025-10-15 18:59:13 -0500

    Agregar ruta de vehicletypes

--- routes/andim.php
+++ routes/andim.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\AdminController;
 use App\Http\Controllers\admin\BrandController;
 use App\Http\Controllers\admin\BrandModelController;
+use App\Http\Controllers\admin\VehicleTypeController;
 
 Route::get('/',[AdminController::class,'index'])->name('admin.index');
 Route::resource('brands', BrandController::class)->names('admin.brands');
 Route::resource('brandmodel', BrandmodelController::class)->names('admin.models');
+Route::resource('vehicletypes', VehicleTypeController::class)->names('admin.vehicletypes');
