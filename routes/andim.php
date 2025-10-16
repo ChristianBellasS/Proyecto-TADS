62f0583b3f5bafd8c0a9eca82047cc1475c92e79
BellasS <cpbellasmil@gmail.com>
2025-10-15 21:44:26 -0500

    Agregar item vehicles en sidebar + ruta

--- routes/andim.php
+++ routes/andim.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\admin\BrandController;
 use App\Http\Controllers\admin\BrandModelController;
 use App\Http\Controllers\admin\VehicleTypeController;
 use App\Http\Controllers\admin\ColorController;
+use App\Http\Controllers\admin\VehicleController;
 
 Route::get('/',[AdminController::class,'index'])->name('admin.index');
 Route::resource('brands', BrandController::class)->names('admin.brands');
 Route::resource('brandmodel', BrandmodelController::class)->names('admin.models');
 Route::resource('vehicletypes', VehicleTypeController::class)->names('admin.vehicletypes');
 Route::resource('colors', ColorController::class)->names('admin.colors');
+Route::resource('vehicles', VehicleController::class)->names('admin.vehicles');
