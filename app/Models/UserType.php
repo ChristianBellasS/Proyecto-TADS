89e6f3f551f6eeea82f6f9e9ffcaebb6392d4571
BellasS <cpbellasmil@gmail.com>
2025-10-18 15:24:49 -0500

    Models userType

---
+++ app/Models/UserType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserType extends Model
+{
+    use HasFactory;
+
+    protected $table = 'usertypes';
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    // Relación con usuarios
+    public function users()
+    {
+        return $this->hasMany(User::class, 'usertype_id');
+    }
+}
